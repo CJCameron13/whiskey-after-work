eeb3f1160df53c9584c9c132174d270630eba745
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-10-01 14:23:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 const navContainer = document.querySelectorAll('.nav-item-container')
+const hoverLine = document.querySelectorAll('.hover-line')
+
+navContainer.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        // Find the hover-line element within the current nav-item-container
+        const hoverLine = item.querySelector('.hover-line'); 
+
+        // Remove the 'hidden' class from the found hover-line
+        hoverLine.classList.remove('hidden'); 
+    });
+});
+
+navContainer.forEach((item) => {
+    item.addEventListener('mouseleave', () => {
+        const hoverLine = item.querySelector('.hover-line'); 
+
+        // Add the 'hidden' class from the found hover-line
+        hoverLine.classList.add('hidden'); 
+    });
+});
