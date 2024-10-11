010179d03ffc11a016add9f60e458500475e75ba
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-10-11 14:34:17 -0400

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,11 +1,24 @@
 const nextArrow = document.querySelector('#next-arrow-container')
+const allPosts = document.querySelectorAll('.post-container')
 
 const madeFromScratch = document.querySelector('#made-from-scratch')
-const theFirstTasting = document.querySelector('#theFirstTasting')
+const theFirstTasting = document.querySelector('#the-first-tasting')
 
 let blogPosts = [madeFromScratch, theFirstTasting]
 
 nextArrow.addEventListener('click', () => {
-    console.log(blogPosts[0])
+    let i = 0
+    i++
+
+    allPosts.forEach((post) => {
+        post.classList.remove('active')
+        post.classList.add('hidden')
+    })
+
+    blogPosts[i].classList.add('active')
+    blogPosts[i].classList.remove('hidden')
+    blogPosts[i].classList.remove('next-up')
+
+    console.log(blogPosts[i])
 })
 
