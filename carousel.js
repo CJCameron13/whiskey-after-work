f02945698d59c9cf239d72914680511e2d196a06
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-10-22 20:23:22 -0400

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,24 +1,51 @@
-const nextArrow = document.querySelector('#next-arrow-container')
-const allPosts = document.querySelectorAll('.post-container')
+const initSlider = () => {
+    const slideButtons = document.querySelectorAll('.slide-button')
+    const imageList = document.querySelector('#post-list')
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth
 
-const madeFromScratch = document.querySelector('#made-from-scratch')
-const theFirstTasting = document.querySelector('#the-first-tasting')
-
-let blogPosts = [madeFromScratch, theFirstTasting]
-
-nextArrow.addEventListener('click', () => {
-    let i = 0
-    i++
-
-    allPosts.forEach((post) => {
-        post.classList.remove('active')
-        post.classList.add('hidden')
+    slideButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const direction = button.id === "prev-slide" ? -1 : 1
+            const scrollAmount = imageList.clientWidth * direction //clientWidth returns the viewable width of an element in pixels
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'})
+            console.log(scrollAmount)
+            console.log(direction)
+        })
     })
 
-    blogPosts[i].classList.add('active')
-    blogPosts[i].classList.remove('hidden')
-    blogPosts[i].classList.remove('next-up')
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? 'none' : 'block'
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? 'none' : 'block'
+    }
 
-    console.log(blogPosts[i])
-})
+    imageList.addEventListener('scroll', () => {
+        handleSlideButtons();
+    })
+}
+
+window.addEventListener('load', initSlider)
+
+// const nextArrow = document.querySelector('#next-arrow-container')
+// const allPosts = document.querySelectorAll('.post-container')
+
+// const madeFromScratch = document.querySelector('#made-from-scratch')
+// const theFirstTasting = document.querySelector('#the-first-tasting')
+
+// let blogPosts = [madeFromScratch, theFirstTasting]
+
+// nextArrow.addEventListener('click', () => {
+//     let i = 0
+//     i++
+
+//     allPosts.forEach((post) => {
+//         post.classList.remove('active')
+//         post.classList.add('hidden')
+//     })
+    
+//     blogPosts[i].classList.add('active')
+//     blogPosts[i].classList.remove('hidden')
+//     blogPosts[i].classList.remove('next-up')
+
+//     console.log(blogPosts[i])
+// })
 
