4e6585486b8d2b7820486d2c5cdb355f33c26ed2
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-10-29 17:45:09 -0400

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -64,27 +64,3 @@ const initSlider = () => {
 
 window.addEventListener('load', initSlider)
 
-// const nextArrow = document.querySelector('#next-arrow-container')
-// const allPosts = document.querySelectorAll('.post-container')
-
-// const madeFromScratch = document.querySelector('#made-from-scratch')
-// const theFirstTasting = document.querySelector('#the-first-tasting')
-
-// let blogPosts = [madeFromScratch, theFirstTasting]
-
-// nextArrow.addEventListener('click', () => {
-//     let i = 0
-//     i++
-
-//     allPosts.forEach((post) => {
-//         post.classList.remove('active')
-//         post.classList.add('hidden')
-//     })
-    
-//     blogPosts[i].classList.add('active')
-//     blogPosts[i].classList.remove('hidden')
-//     blogPosts[i].classList.remove('next-up')
-
-//     console.log(blogPosts[i])
-// })
-
