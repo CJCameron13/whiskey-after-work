02e7e23b41d6daa44af154998c8f0c9b3154cd25
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-11-18 18:25:47 -0500

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,9 +1,34 @@
+const typeButtonContainer = document.querySelector('.type-selector')
 const typeButton = document.querySelectorAll('.type')
+const bourbonPage = document.querySelector('#bourbon')
+const scotchPage = document.querySelector('#scotch')
+const ryePage = document.querySelector('#rye')
 
-typeButton.forEach((type) => {
-    type.addEventListener('click', (event) => {
-        let selected = event.target
-        console.log(selected)
-        selected.classList.add('active')
-    })
-})
+typeButtonContainer.addEventListener('click', (event) => {
+    const selected = event.target.closest('.type'); // Find the closest ancestor with the class 'type'
+  
+    if (selected) { 
+      console.log(selected);
+      typeButton.forEach((type) => {
+        type.classList.remove('active')
+      })
+      selected.classList.add('active'); 
+    }
+
+    if (selected.classList.contains('bourbon')) {
+        console.log('bourbon')
+        scotchPage.classList.add('hidden')
+        ryePage.classList.add('hidden')
+        bourbonPage.classList.remove('hidden')
+    } else if (selected.classList.contains('scotch')) {
+        console.log('scotch')
+        bourbonPage.classList.add('hidden')
+        ryePage.classList.add('hidden')
+        scotchPage.classList.remove('hidden')
+    } else if (selected.classList.contains('rye')) {
+        console.log('rye')
+        bourbonPage.classList.add('hidden')
+        scotchPage.classList.add('hidden')
+        ryePage.classList.remove('hidden')
+    }
+  });
