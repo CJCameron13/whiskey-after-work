f59ab7323a9a6e04e8c30119784f080e7756f286
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-11-25 21:11:42 -0500

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -32,3 +32,28 @@ typeButtonContainer.addEventListener('click', (event) => {
         ryePage.classList.remove('hidden')
     }
   });
+
+const whiskeyCards = document.querySelectorAll('.card')
+const listContainer = document.querySelectorAll('.list-container')
+const highlightImages = document.querySelectorAll('.highlight-image')
+
+const angelsEnvyStraightHighlight = document.querySelector('#angels-envy-straight-highlight')
+const fourRosesSmallBatch = document.querySelector('#four-roses-small-batch-highlight')
+
+listContainer.forEach((container) => {
+  container.addEventListener('click', (event) => {
+    const chosen = event.target.closest('.card')
+    if (chosen.classList.contains('angels-envy-straight')) {
+      highlightImages.forEach((highlight) => {
+        highlight.classList.add('hidden')
+      });
+      console.log(angelsEnvyStraightHighlight)
+      return angelsEnvyStraightHighlight.classList.remove('hidden')
+    } else if (chosen.classList.contains('four-roses-small-batch')) {
+      highlightImages.forEach((highlight) => {
+        highlight.classList.add('hidden')
+      });
+      fourRosesSmallBatch.classList.remove('hidden')
+    }
+  })
+})
