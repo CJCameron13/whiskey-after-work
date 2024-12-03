1514135ed59bb875c1ee9a59a6a6dd8bcc513bf1
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-12-02 20:21:29 -0500

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -35,25 +35,37 @@ typeButtonContainer.addEventListener('click', (event) => {
 
 const whiskeyCards = document.querySelectorAll('.card')
 const listContainer = document.querySelectorAll('.list-container')
-const highlightImages = document.querySelectorAll('.highlight-image')
+const highlightImagesBourbon = document.querySelectorAll('.highlight-image.bourbon')
 
 const angelsEnvyStraightHighlight = document.querySelector('#angels-envy-straight-highlight')
 const fourRosesSmallBatch = document.querySelector('#four-roses-small-batch-highlight')
+const elijahCraigSmallBatch = document.querySelector('#elijah-craig-small-batch-highlight')
+const hunterAndScottBourbon = document.querySelector('#hunter-and-scott-bourbon-highlight')
 
 listContainer.forEach((container) => {
   container.addEventListener('click', (event) => {
     const chosen = event.target.closest('.card')
     if (chosen.classList.contains('angels-envy-straight')) {
-      highlightImages.forEach((highlight) => {
+      highlightImagesBourbon.forEach((highlight) => {
         highlight.classList.add('hidden')
       });
       console.log(angelsEnvyStraightHighlight)
-      return angelsEnvyStraightHighlight.classList.remove('hidden')
+      angelsEnvyStraightHighlight.classList.remove('hidden')
     } else if (chosen.classList.contains('four-roses-small-batch')) {
-      highlightImages.forEach((highlight) => {
+      highlightImagesBourbon.forEach((highlight) => {
         highlight.classList.add('hidden')
       });
       fourRosesSmallBatch.classList.remove('hidden')
+    } else if (chosen.classList.contains('elijah-craig-small-batch')) {
+      highlightImagesBourbon.forEach((highlight) => {
+        highlight.classList.add('hidden')
+      });
+      elijahCraigSmallBatch.classList.remove('hidden')
+    } else if (chosen.classList.contains('hunter-and-scott-bourbon')) {
+      highlightImagesBourbon.forEach((highlight) => {
+        highlight.classList.add('hidden')
+      });
+      hunterAndScottBourbon.classList.remove('hidden')
     }
   })
 })
